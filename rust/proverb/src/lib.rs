c825cfd047922e066a8bcadee86ae41aec13cb19
Cory Stegelmeier <cstegel@gmail.com>
2018-10-27 19:20:57 -0400

    Improve proverb solution using windows & join

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -7,18 +7,20 @@
 /// For want of a message the battle was lost.
 /// For want of a battle the kingdom was lost.
 /// And all for the want of a nail.
+extern crate itertools;
+
+use itertools::Itertools;
 use std::iter::once;
 
 pub fn build_proverb(list: Vec<&str>) -> String {
-    match list.len() {
-        0 => String::new(),
-        _ => list[..list.len() - 1]
-            .iter()
-            .zip(list.iter().cycle().skip(1))
-            .map(|(first, second)| format!("For want of a {} the {} was lost.\n", first, second))
-            .chain(once(format!(
-                "And all for the want of a {}.",
-                list.get(0).unwrap()
-            ))).collect(),
+    if list.is_empty() {
+        return String::new();
     }
+
+    list.windows(2)
+        .map(|win| format!("For want of a {} the {} was lost.", win[0], win[1]))
+        .chain(once(format!(
+            "And all for the want of a {}.",
+            list.get(0).unwrap()
+        ))).join("\n")
 }
