4ba4a5769b30a414ee2ed1e8bf351487943f595d
Cory Stegelmeier <cstegel@gmail.com>
2018-10-28 15:21:33 -0400

    Add solution to "sum-of-multiples"

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,7 +1,67 @@
-pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+extern crate itertools;
+
+// Can uncomment this to use itertools "unique" adaptor which should behave the same
+// I implemented the "unique" extension to "Iterator" trait as a learning exercise
+// use itertools::Itertools;
+
+use std::cmp::Eq;
+use std::collections::HashSet;
+use std::hash::Hash;
+
+#[derive(Debug)]
+struct Unique<I>
+where
+    I: Iterator,
+    I::Item: Eq + Hash,
+{
+    iter: I,
+    seen_mults: HashSet<I::Item>,
+}
+
+impl<I> Iterator for Unique<I>
+where
+    I: Iterator,
+    I::Item: Eq + Hash + Clone,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<I::Item> {
+        loop {
+            match self.iter.next() {
+                None => return None,
+                Some(v) => {
+                    if self.seen_mults.contains(&v) {
+                        continue;
+                    }
+                    self.seen_mults.insert(v.clone());
+                    return Some(v);
+                }
+            }
+        }
+    }
+}
+
+trait UniqueExt: Iterator {
+    fn unique(self) -> Unique<Self>
+    where
+        Self::Item: Hash + Eq + Clone,
+        Self: Sized,
+    {
+        Unique {
+            iter: self,
+            seen_mults: HashSet::new(),
+        }
+    }
+}
+
+impl<I: Iterator> UniqueExt for I {}
+fn unique_multiples<'a>(limit: u32, factors: &'a [u32]) -> impl Iterator<Item = u32> + 'a {
+    factors
+        .iter()
+        .flat_map(move |f| (*f..limit).step_by(*f as usize))
+        .unique()
+}
+
+pub fn sum_of_multiples<'a>(limit: u32, factors: &'a [u32]) -> u32 {
+    unique_multiples(limit, factors).sum()
 }
