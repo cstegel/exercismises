afc8afca8bb6f1fe7ac817bf7f575d34a1e1548c
Cory Stegelmeier <cstegel@gmail.com>
2018-11-15 22:20:43 -0500

    Add solution for "armstrong-numbers"

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,36 @@
+#[derive(Debug)]
+struct Digits {
+    num: u32,
+    done: bool,
+}
+
+impl Digits {
+    fn new(num: u32) -> Digits {
+        Digits { num, done: false }
+    }
+}
+
+impl Iterator for Digits {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.done {
+            false => {
+                let digit = self.num % 10;
+                self.num /= 10;
+                self.done = self.num == 0;
+                Some(digit)
+            }
+            true => None,
+        }
+    }
+}
+
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let digits: Vec<_> = Digits::new(num).collect();
+    digits
+        .iter()
+        .map(|digit| digit.pow(digits.len() as u32))
+        .sum::<u32>()
+        == num
 }
