01ecd55e5c955c7f7ea30917d37c337c2f53c22b
Cory Stegelmeier <cstegel@gmail.com>
2018-11-19 21:15:26 -0500

    Add  recursive solution to "collatz-conjecture"

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,16 +1,27 @@
-pub fn collatz(mut n: u64) -> Option<u64> {
-    if n == 0 {
-        return None;
+pub fn collatz(n: u64) -> Option<u64> {
+    // recursive solution
+    match n {
+        0 => None,
+        1 => Some(0),
+        _ => match n {
+            _ if n % 2 == 0 => collatz(n / 2),
+            _ => collatz(3 * n + 1),
+        }.map(|x| x + 1),
     }
-    let mut steps = 0;
-    loop {
-        if n == 1 {
-            return Some(steps);
-        }
-        match n % 2 {
-            0 => n /= 2,
-            _ => n = 3 * n + 1,
-        };
-        steps += 1;
-    }
+
+    // Below is the iterative solution
+    // if n == 0 {
+    //     return None;
+    // }
+    // let mut steps = 0;
+    // loop {
+    //     if n == 1 {
+    //         return Some(steps);
+    //     }
+    //     match n % 2 {
+    //         0 => n /= 2,
+    //         _ => n = 3 * n + 1,
+    //     };
+    //     steps += 1;
+    // }
 }
