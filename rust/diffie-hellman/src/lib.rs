ee0a1b96ed603c6902e80ae9c4d7daec93796736
Cory Stegelmeier <cstegel@gmail.com>
2018-11-21 22:40:29 -0500

    Add solution to "diffie-hellman"

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,21 +1,34 @@
+extern crate rand;
+
+use rand::thread_rng;
+use rand::Rng;
+
+///
+/// Calculate a base raised to an exponent under a modulus
+/// O(log exp) run time
+/// O(log exp) memory for recursive stack frames
+fn exp_mod(base: u64, exp: u64, modulus: u64) -> u64 {
+    (match exp {
+        0 => 1,
+        1 => base,
+        _ if exp % 2 == 0 => exp_mod(base, exp / 2, modulus).pow(2),
+        _ => {
+            let floor_half = exp / 2;
+            exp_mod(base, floor_half, modulus) * exp_mod(base, exp - floor_half, modulus)
+        }
+    }) % modulus
+}
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    thread_rng().gen_range(1, p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate public key using prime numbers {} and {}, and private key {}",
-        p,
-        g,
-        a
-    )
+    // pub = g**a mod p
+    exp_mod(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate secret key using prime number {}, public key {}, and private key {}",
-        p,
-        b_pub,
-        a
-    )
+    // s = b_pub**a mod p
+    exp_mod(b_pub, a, p)
 }
