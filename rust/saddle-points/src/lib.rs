a23a839a5b8965b1509d805e1550fa15aad313e0
Cory Stegelmeier <cstegel@gmail.com>
2018-11-25 22:41:03 -0500

    Add solution for "saddle-points"

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,6 +1,27 @@
+use std::vec::Vec;
+
+/// Find points where value is >= all in row and <= all in col
+/// input: slice of rows
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    unimplemented!(
-        "find the saddle points of the following matrix: {:?}",
-        input
-    )
+    if input.is_empty() || input[0].is_empty() {
+        return Vec::new();
+    }
+
+    let n_rows = input.len();
+    let n_cols = input[0].len();
+
+    let row_maxes: Vec<u64> = input
+        .iter()
+        .map(|row| *row.iter().max().expect("All rows must be same size"))
+        .collect();
+    let col_mins: Vec<u64> = (0..n_cols)
+        .map(|c| input.iter().map(|row| row[c]).min().unwrap())
+        .collect();
+
+    (0..n_rows)
+        .flat_map(|r| (0..n_cols).map(move |c| (r, c)))
+        .filter(|(r, c)| {
+            let v = input[*r][*c];
+            v == row_maxes[*r] && v == col_mins[*c]
+        }).collect()
 }
