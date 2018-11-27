a36e4034e0f42dab1267469222e806e437d44523
Cory Stegelmeier <cstegel@gmail.com>
2018-11-26 21:03:47 -0500

    Add solution to "isogram"

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,3 +1,14 @@
+use std::collections::HashSet;
+
+fn duplicate_chars<'a>(candidate: &'a str) -> impl Iterator<Item = char> + 'a {
+    let mut found: HashSet<String> = HashSet::new();
+    candidate
+        .chars()
+        .filter(move |c| !found.insert(c.to_lowercase().to_string()))
+}
+
+/// Check if candidate is an isogram
+/// Works for unicode strings
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    duplicate_chars(candidate).all(|c| !c.is_alphanumeric())
 }
