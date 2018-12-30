1709f3c1cef7d4f8e88874536c75685f9e2fcbb7
Cory Stegelmeier <cstegel@gmail.com>
2018-12-30 15:25:12 -0500

    Add soln to "hamming"

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,5 +1,8 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    unimplemented!("What is the Hamming Distance between {} and {}", s1, s2);
+    if s1.len() != s2.len() {
+        return None;
+    }
+    Some(s1.chars().zip(s2.chars()).filter(|(a, b)| a != b).count())
 }
