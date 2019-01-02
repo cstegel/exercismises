a254705d00248aa54fadb9f2f5f1b9f23939a810
Cory Stegelmeier <cstegel@gmail.com>
2019-01-01 22:11:41 -0500

    Add soln for "pascals-triangle"

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,11 +1,34 @@
-pub struct PascalsTriangle;
+use std::iter;
+
+pub struct PascalsTriangle {
+    rows: Vec<Vec<u32>>,
+}
+
+fn create_next_row(prev: &Vec<u32>) -> Vec<u32> {
+    let mut row: Vec<u32> = iter::repeat(1).take(prev.len() + 1).collect();
+
+    let splice_range = 1..row.len() - 1;
+    row.splice(
+        splice_range.clone(),
+        splice_range.map(|i| prev[i - 1] + prev[i]),
+    );
+    row
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        let mut rows: Vec<Vec<u32>> = iter::repeat(Vec::new()).take(row_count as usize).collect();
+        if row_count > 0 {
+            rows[0] = vec![1];
+        }
+        for r in 1..row_count as usize {
+            rows[r] = create_next_row(&rows[r - 1]);
+        }
+
+        Self { rows }
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        self.rows.iter().cloned().collect()
     }
 }
