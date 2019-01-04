450026b55f8e1034a5ba1f0023f1adbad95bba25
Cory Stegelmeier <cstegel@gmail.com>
2019-01-03 22:14:16 -0500

    Add soln to "scrabble-score"

--- rust/scrabble-score/src/lib.rs
+++ rust/scrabble-score/src/lib.rs
@@ -1,4 +1,17 @@
+fn score_char(c: &char) -> u64 {
+    match c.to_ascii_lowercase() {
+        'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+        'd' | 'g' => 2,
+        'b' | 'c' | 'm' | 'p' => 3,
+        'f' | 'h' | 'v' | 'w' | 'y' => 4,
+        'k' => 5,
+        'j' | 'x' => 8,
+        'q' | 'z' => 10,
+        _ => 0,
+    }
+}
+
 /// Compute the Scrabble score for a word.
 pub fn score(word: &str) -> u64 {
-    unimplemented!("Score {} in Scrabble.", word);
+    word.chars().map(|c| score_char(&c)).sum()
 }
