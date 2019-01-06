6eb684b58063b723aeb9cf070df2a2405a6b4308
Cory Stegelmeier <cstegel@gmail.com>
2019-01-05 23:03:37 -0500

    Add soln to "luhn"

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,4 +1,28 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    let rev_no_white = || code.chars().rev().filter(|c| !c.is_whitespace());
+
+    // don't allow invalid digits or sequences of <= 1 digit
+    if rev_no_white().any(|c| !c.is_digit(10) || rev_no_white().count() <= 1) {
+        return false;
+    }
+
+    rev_no_white()
+        .map(|c| c.to_digit(10).unwrap())
+        .enumerate()
+        .map(|(i, digit)| {
+            if i % 2 == 0 {
+                digit
+            } else {
+                let prod = 2 * digit;
+                if prod > 9 {
+                    prod - 9
+                } else {
+                    prod
+                }
+            }
+        })
+        .sum::<u32>()
+        % 10
+        == 0
 }
