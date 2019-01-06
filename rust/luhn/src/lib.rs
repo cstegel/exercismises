62de336b36259a190286004a8e8d61e83b6609d6
Cory Stegelmeier <cstegel@gmail.com>
2019-01-05 23:42:54 -0500

    Change "luhn" soln to be pure functional

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,28 +1,24 @@
 /// Check a Luhn checksum.
+/// Inspired by JaneL's brilliant solution here:
+/// https://exercism.io/tracks/rust/exercises/luhn/solutions/b310e5203500486398d482b47561cac1
 pub fn is_valid(code: &str) -> bool {
-    let rev_no_white = || code.chars().rev().filter(|c| !c.is_whitespace());
-
-    // don't allow invalid digits or sequences of <= 1 digit
-    if rev_no_white().any(|c| !c.is_digit(10) || rev_no_white().count() <= 1) {
-        return false;
-    }
-
-    rev_no_white()
-        .map(|c| c.to_digit(10).unwrap())
-        .enumerate()
-        .map(|(i, digit)| {
-            if i % 2 == 0 {
-                digit
-            } else {
-                let prod = 2 * digit;
-                if prod > 9 {
-                    prod - 9
-                } else {
-                    prod
-                }
-            }
+    code.chars()
+        .rev()
+        .filter(|c| !c.is_whitespace())
+        .try_fold((0, 0), |(acc, n_digits), digit_candidate| {
+            digit_candidate
+                .to_digit(10)
+                .map(|num| {
+                    if n_digits % 2 == 0 {
+                        num
+                    } else {
+                        match 2 * num {
+                            prod if prod > 9 => prod - 9,
+                            prod => prod,
+                        }
+                    }
+                })
+                .map(|num| (acc + num, n_digits + 1))
         })
-        .sum::<u32>()
-        % 10
-        == 0
+        .map_or(false, |(sum, n_digits)| sum % 10 == 0 && n_digits > 1)
 }
