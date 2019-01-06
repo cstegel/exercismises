4c2cdf4f56d5be08aa05ffad8182191c4fbe08bc
Cory Stegelmeier <cstegel@gmail.com>
2019-01-05 22:10:01 -0500

    Change "nucleotide-count" to be more functional

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,31 +1,24 @@
 use std::collections::HashMap;
 
-use std::collections::HashSet;
+static NUCLEOTIDES: [char; 4] = ['A', 'C', 'G', 'T'];
 
-#[macro_use]
-extern crate lazy_static;
-
-lazy_static! {
-    static ref valid_nucleotides: HashSet<char> = "ACGT".chars().collect();
+fn validate(nucleotide: char) -> Result<char, char> {
+    NUCLEOTIDES
+        .iter()
+        .cloned()
+        .find(|c| *c == nucleotide)
+        .ok_or(nucleotide)
 }
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    if !valid_nucleotides.contains(&nucleotide) {
-        return Err(nucleotide);
-    }
-
-    // check that all nucleotides are valid
-    if let Some(invalid) = dna.chars().find(|c| !valid_nucleotides.contains(c)) {
-        return Err(invalid);
-    }
-
+    validate(nucleotide)?;
+    dna.chars().map(validate).collect::<Result<Vec<_>, _>>()?;
     Ok(dna.chars().filter(|c| *c == nucleotide).count())
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    let mut counts: HashMap<char, usize> = HashMap::new();
-    for nucleotide in valid_nucleotides.iter() {
-        counts.insert(*nucleotide, count(*nucleotide, dna)?);
-    }
-    Ok(counts)
+    NUCLEOTIDES
+        .iter()
+        .map(|nuc| count(*nuc, dna).map(|size| (*nuc, size)))
+        .collect()
 }
