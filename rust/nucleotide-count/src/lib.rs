4e4b23e41d707a6f25a75ff7c96af00c90c9d324
Cory Stegelmeier <cstegel@gmail.com>
2019-01-05 21:22:42 -0500

    Add soln to "nucleotide-count"

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,16 +1,31 @@
 use std::collections::HashMap;
 
+use std::collections::HashSet;
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref valid_nucleotides: HashSet<char> = "ACGT".chars().collect();
+}
+
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    unimplemented!(
-        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
-        nucleotide,
-        dna
-    );
+    if !valid_nucleotides.contains(&nucleotide) {
+        return Err(nucleotide);
+    }
+
+    // check that all nucleotides are valid
+    if let Some(invalid) = dna.chars().find(|c| !valid_nucleotides.contains(c)) {
+        return Err(invalid);
+    }
+
+    Ok(dna.chars().filter(|c| *c == nucleotide).count())
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!(
-        "How much of every nucleotide type is contained inside DNA string '{}'?",
-        dna
-    );
+    let mut counts: HashMap<char, usize> = HashMap::new();
+    for nucleotide in valid_nucleotides.iter() {
+        counts.insert(*nucleotide, count(*nucleotide, dna)?);
+    }
+    Ok(counts)
 }
