6363869d3289abbe245b1592f5c187815adf0e72
Cory Stegelmeier <cstegel@gmail.com>
2019-01-06 18:30:08 -0500

    Add soln for "largest-series-product"

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -5,9 +5,29 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    unimplemented!(
-        "largest series product of a span of {} digits in {}",
-        span,
-        string_digits
-    );
+    if span > string_digits.len() {
+        return Err(Error::SpanTooLong);
+    }
+    if span == 0 {
+        // tests say 1 is expected return value of 0-length span
+        return Ok(1);
+    }
+
+    let digits = match string_digits
+        .chars()
+        .map(|c| c.to_digit(10).ok_or(c))
+        .collect::<Result<Vec<_>, char>>()
+    {
+        Err(invalid_char) => return Err(Error::InvalidDigit(invalid_char)),
+        Ok(digits) => digits,
+    };
+
+    match digits
+        .windows(span)
+        .map(|window| window.iter().product::<u32>())
+        .max()
+    {
+        Some(max_prod) => Ok(max_prod as u64),
+        None => panic!("Shouldn't get here"),
+    }
 }
