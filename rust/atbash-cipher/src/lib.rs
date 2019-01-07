cd64b5d45d8a1c9a25ca6bd63d9fb8d1d1f9793d
Cory Stegelmeier <cstegel@gmail.com>
2019-01-06 21:52:29 -0500

    Add soln to "atbash-cipher"

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,9 +1,32 @@
+use itertools::Itertools;
+
+pub fn encode_char(c: char) -> char {
+    if c.is_numeric() {
+        c
+    } else {
+        let pos_from_a = c.to_ascii_lowercase() as u8 - ('a' as u8);
+        (('z' as u8) - pos_from_a) as char
+    }
+}
+
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    plain
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(encode_char)
+        .collect::<Vec<char>>()
+        .chunks(5)
+        .intersperse(&[' '])
+        .flatten()
+        .collect()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    unimplemented!("Decoding of {:?} in Atbash cipher.", cipher);
+    cipher
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .map(encode_char)
+        .collect()
 }
