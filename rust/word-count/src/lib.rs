6eb680100a65806c97a290e6cf316e94e49f262b
Cory Stegelmeier <cstegel@gmail.com>
2019-01-06 20:55:11 -0500

    Refactor "word-count" to be smaller

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,20 +1,16 @@
 use std::collections::HashMap;
 
-use itertools::Itertools;
-
 /// Count occurrences of words.
 /// A word is identified as an adjacent grouping of alphanumeric characters and apostraphes
 /// in their lower-case form except apostraphes cannot be the first or last char in the word
 pub fn word_count(sentence: &str) -> HashMap<String, u32> {
     sentence
         .to_lowercase()
-        .chars()
-        .group_by(|c| c.is_alphanumeric() || *c == '\'')
-        .into_iter()
-        .filter(|(is_word, _)| *is_word)
-        .map(|(_, char_iter)| char_iter.collect::<String>().trim_matches('\'').to_string())
+        .split(|c: char| !c.is_alphanumeric() && c != '\'')
+        .map(|substr| substr.trim_matches('\''))
+        .filter(|substr| !substr.is_empty())
         .fold(HashMap::new(), |mut counts, word| {
-            *counts.entry(word).or_insert(0) += 1;
+            *counts.entry(word.to_string()).or_insert(0) += 1;
             counts
         })
 }
