30b4777507b987e6ef7fd4ff6dbdf44c4ef58716
Cory Stegelmeier <cstegel@gmail.com>
2019-01-14 21:57:41 -0500

    Add soln to "rotational-cipher"

--- rust/rotational-cipher/src/lib.rs
+++ rust/rotational-cipher/src/lib.rs
@@ -1,7 +1,14 @@
+pub fn encode_char(c: char, key: i8) -> char {
+    if c.is_ascii_alphabetic() {
+        let start_char = if c.is_ascii_lowercase() { 'a' } else { 'A' };
+
+        let pos_from_start = (c as u8) - (start_char as u8);
+        ((start_char as u8) + ((pos_from_start as i16 + key as i16) % 26) as u8) as char
+    } else {
+        c
+    }
+}
+
 pub fn rotate(input: &str, key: i8) -> String {
-    unimplemented!(
-        "How would input text '{}' transform when every letter is shifted using key '{}'?",
-        input,
-        key
-    );
+    input.chars().map(|c| encode_char(c, key)).collect()
 }
