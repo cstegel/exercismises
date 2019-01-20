8f2b40ed5d3cee58e50d268f671b56f36a222ed3
Cory Stegelmeier <cstegel@gmail.com>
2019-01-19 23:17:54 -0500

    Add solution to "acronym"

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,3 +1,60 @@
+enum LastChar {
+    Nothing,
+    Lowercase,
+    Uppercase,
+    Separator,
+}
+
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    let mut prev_char = LastChar::Nothing;
+
+    let mut acronym = String::new();
+    let alphabetic_state = |ch: char| {
+        if ch.is_uppercase() {
+            LastChar::Uppercase
+        } else {
+            LastChar::Lowercase
+        }
+    };
+
+    let to_acro_char = |ch: char| ch.to_uppercase().collect::<String>();
+
+    for ch in phrase.chars() {
+        prev_char = match prev_char {
+            LastChar::Nothing => {
+                if ch.is_alphabetic() {
+                    acronym.push_str(to_acro_char(ch).as_str());
+                    alphabetic_state(ch)
+                } else {
+                    LastChar::Separator
+                }
+            }
+            LastChar::Lowercase => {
+                if ch.is_alphabetic() {
+                    if ch.is_uppercase() {
+                        acronym.push_str(to_acro_char(ch).as_str());
+                    }
+                    alphabetic_state(ch)
+                } else {
+                    LastChar::Separator
+                }
+            }
+            LastChar::Uppercase => {
+                if ch.is_alphabetic() {
+                    alphabetic_state(ch)
+                } else {
+                    LastChar::Separator
+                }
+            }
+            LastChar::Separator => {
+                if ch.is_alphabetic() {
+                    acronym.push_str(to_acro_char(ch).as_str());
+                    alphabetic_state(ch)
+                } else {
+                    LastChar::Separator
+                }
+            }
+        }
+    }
+    acronym
 }
