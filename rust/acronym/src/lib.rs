abe62f02682e8b78fe91832b3b4d80a5f4c3dfbb
Cory Stegelmeier <cstegel@gmail.com>
2019-01-20 17:02:56 -0500

    Add "fold" solution to "acronym"

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -5,7 +5,7 @@ enum LastChar {
     Separator,
 }
 
-pub fn abbreviate(phrase: &str) -> String {
+pub fn abbreviate_state_machine(phrase: &str) -> String {
     let mut prev_char = LastChar::Nothing;
 
     let mut acronym = String::new();
@@ -55,4 +55,27 @@ pub fn abbreviate(phrase: &str) -> String {
         }
     }
     acronym.to_uppercase()
+}
+
+///
+/// Better than abbreviate_state_machine due to less code
+/// while still maintaining a single pass through the string
+/// State machine isn't necessary since only the immediately previous character
+/// is needed to be known
+pub fn abbreviate_fold(phrase: &str) -> String {
+    let (acronym, _) = phrase
+        .chars()
+        .fold((String::new(), ' '), |(mut acronym, prev_char), ch| {
+            if !prev_char.is_alphabetic() && ch.is_alphabetic() {
+                acronym.push(ch);
+            } else if prev_char.is_lowercase() && ch.is_uppercase() {
+                acronym.push(ch);
+            }
+            (acronym, ch)
+        });
+    acronym.to_uppercase()
+}
+
+pub fn abbreviate(phrase: &str) -> String {
+    abbreviate_fold(phrase)
 }
