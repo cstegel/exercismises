9c0eb211d3327d1ddde2f485d681cbe36a86a785
Cory Stegelmeier <cstegel@gmail.com>
2019-01-27 16:53:58 -0500

    Remove duplicate code

--- rust/triangle/src/lib.rs
+++ rust/triangle/src/lib.rs
@@ -4,18 +4,20 @@ pub struct Triangle {
     sides: [u64; 3],
 }
 
+fn side_windows_2(sides: &[u64; 3]) -> impl Iterator<Item = (&u64, &u64)> {
+    sides.iter().cycle().tuple_windows().take(3)
+}
+
+fn side_windows_3(sides: &[u64; 3]) -> impl Iterator<Item = (&u64, &u64, &u64)> {
+    sides.iter().cycle().tuple_windows().take(3)
+}
+
 impl Triangle {
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
         if sides.iter().any(|x| *x == 0) {
             None
         } else {
-            if sides
-                .iter()
-                .cycle()
-                .tuple_windows()
-                .take(3)
-                .any(|(x, y, z)| x + y < *z)
-            {
+            if side_windows_3(&sides).any(|(x, y, z)| x + y < *z) {
                 None
             } else {
                 Some(Triangle { sides })
@@ -23,32 +25,15 @@ impl Triangle {
         }
     }
 
-    // fn side_combos<'a>(&'a self) -> impl Iterator<Item = u64> {
-    //     // impl Iterator<Item = <std::slice::Iter<'_, u64> as Iterator>::Item>
-    //     // itertools::tuple_impl::TupleWindows<std::slice::Iter<'_, u64>, _>
-    //     // self.sides.iter().cycle().tuple_windows().take(3)
-    //     self.sides.iter().tuple_windows()
-    // }
-
     pub fn is_equilateral(&self) -> bool {
         self.sides.iter().all(|x| *x == self.sides[0])
     }
 
     pub fn is_scalene(&self) -> bool {
-        self.sides
-            .iter()
-            .cycle()
-            .tuple_windows()
-            .take(3)
-            .all(|(x, y)| x != y)
+        side_windows_2(&self.sides).all(|(x, y)| x != y)
     }
 
     pub fn is_isosceles(&self) -> bool {
-        self.sides
-            .iter()
-            .cycle()
-            .tuple_windows()
-            .take(3)
-            .any(|(x, y)| x == y)
+        side_windows_2(&self.sides).any(|(x, y)| x == y)
     }
 }
