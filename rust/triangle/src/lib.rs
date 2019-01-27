93d227cc93531e6efc430e6cba29ba4c4d422394
Cory Stegelmeier <cstegel@gmail.com>
2019-01-27 17:09:00 -0500

    Change "triangle" solution to be generic over type

--- rust/triangle/src/lib.rs
+++ rust/triangle/src/lib.rs
@@ -1,23 +1,30 @@
 use itertools::Itertools;
 
-pub struct Triangle {
-    sides: [u64; 3],
+use std::cmp::PartialOrd;
+
+use num::Num;
+
+pub struct Triangle<T> {
+    sides: [T; 3],
 }
 
-fn side_windows_2(sides: &[u64; 3]) -> impl Iterator<Item = (&u64, &u64)> {
+fn side_windows_2<T>(sides: &[T; 3]) -> impl Iterator<Item = (&T, &T)> {
     sides.iter().cycle().tuple_windows().take(3)
 }
 
-fn side_windows_3(sides: &[u64; 3]) -> impl Iterator<Item = (&u64, &u64, &u64)> {
+fn side_windows_3<T>(sides: &[T; 3]) -> impl Iterator<Item = (&T, &T, &T)> {
     sides.iter().cycle().tuple_windows().take(3)
 }
 
-impl Triangle {
-    pub fn build(sides: [u64; 3]) -> Option<Triangle> {
-        if sides.iter().any(|x| *x == 0) {
+impl<T> Triangle<T>
+where
+    T: Num + PartialOrd + From<i32> + Copy,
+{
+    pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
+        if sides.iter().any(|x| *x == T::from(0)) {
             None
         } else {
-            if side_windows_3(&sides).any(|(x, y, z)| x + y < *z) {
+            if side_windows_3(&sides).any(|(x, y, z)| *x + *y < *z) {
                 None
             } else {
                 Some(Triangle { sides })
@@ -29,6 +36,8 @@ impl Triangle {
         self.sides.iter().all(|x| *x == self.sides[0])
     }
 
+    // TODO: how would we pad comparisons by float epsilons
+    // for float but be exact for int types?
     pub fn is_scalene(&self) -> bool {
         side_windows_2(&self.sides).all(|(x, y)| x != y)
     }
